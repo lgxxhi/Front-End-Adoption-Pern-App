7dc86ffd321881e83bb250b52c75d5d388135d29
michael reyes <draftiercrayon4@gmail.com>
2023-08-07 10:25:12 -0400

    added some basic styling

--- src/components/Pets/Pets.js
+++ src/components/Pets/Pets.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import "./Pets.css";
 
 function Pets() {
   let url = process.env.REACT_APP_API_URL;
@@ -23,24 +24,26 @@ function Pets() {
 
   function mapData() {
     return petData.map((pet) => (
-      <div key={pet.id} className="pet-item">
-        <img src={pet.photo} />
-        <h3>{pet.name}</h3>
-        <p>Age: {pet.age}</p>
-        <p>Breed: {pet.breed || "N/A"}</p>
-        <p>Location: {pet.location || "N/A"}</p>
-        <div className="favorite">
-          {pet.is_favorite && <i className="fas fa-heart"></i>}
-        </div>
-        <div className="actions">
-          <Link to={`/pets/${pet.id}`}>View Details</Link>{" "}
-          <Link to={`/pets/${pet.id}/edit`}>Edit</Link>
+      <div key={pet.id} className="pet-card">
+        <img src={pet.photo} alt={pet.name} />
+        <div className="pet-info">
+          <h3>{pet.name}</h3>
+          <p>Age: {pet.age}</p>
+          <p>Breed: {pet.breed || "N/A"}</p>
+          <p>Location: {pet.location || "N/A"}</p>
+          <div className="favorite">
+            {pet.is_favorite && <i className="fas fa-heart"></i>}
+          </div>
+          <div className="actions">
+            <Link to={`/pets/${pet.id}`}>View Details</Link>{" "}
+          </div>
         </div>
       </div>
     ));
   }
+
   return (
-    <div>
+    <div className="pet-container">
       {petData.length === 0 ? (
         <div>Find some pets that need a home!</div>
       ) : (
