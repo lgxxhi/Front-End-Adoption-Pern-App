b7b36aa6bba1f137d48ee5bdf98d4f8cd9e35806
michael reyes <draftiercrayon4@gmail.com>
2023-08-07 12:54:18 -0400

    completed routeand form for adding a new pet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Home from "./components/Home/Home";
 import Pets from "./components/Pets/Pets";
+import AddPet from "./components/AddPet/AddPet";
 import petData from "./data/PetData";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/pets" element={<Pets />} />
+          <Route path="/add-pet" element={<AddPet />} />
           <Route path="/404" element={<h1>404 Not found!</h1>} />
         </Routes>
       </Router>
--- src/components/AddPet/AddPet.js
+++ src/components/AddPet/AddPet.js
@@ -1,7 +1,171 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function AddPet() {
-  return <div>AddPet</div>;
+  const [petData, setPetData] = useState({
+    name: "",
+    age: 0,
+    breed: "",
+    gender: "m",
+    location: "",
+    color: "",
+    size: "",
+    story: "",
+    is_favorite: false,
+    photo: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    // Handle radio buttons separately
+    if (type === "radio") {
+      setPetData({ ...petData, [name]: value });
+    } else {
+      setPetData({ ...petData, [name]: type === "checkbox" ? checked : value });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = process.env.REACT_APP_API_URL;
+      await axios.post(`${url}/pets`, petData);
+      alert("New pet added!");
+      navigate("/pets");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Create New Bookmark</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={petData.name}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Age:
+          <input
+            type="number"
+            name="age"
+            step="1"
+            min={0}
+            value={petData.age}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Breed:
+          <input
+            type="text"
+            name="breed"
+            value={petData.breed}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Gender:
+          <input
+            type="radio"
+            name="gender"
+            value="m"
+            checked={petData.gender === "m"}
+            onChange={handleChange}
+          />
+          M
+        </label>{" "}
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="f"
+            checked={petData.gender === "f"}
+            onChange={handleChange}
+          />
+          F
+        </label>
+        <br />
+        <br />
+        <label>
+          Location:
+          <input
+            type="text"
+            name="location"
+            value={petData.location}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Color:
+          <input
+            type="text"
+            name="color"
+            value={petData.color}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Size:
+          <input
+            type="text"
+            name="size"
+            value={petData.size}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Story:
+          <textarea
+            name="story"
+            value={petData.story}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Is Favorite:
+          <input
+            type="checkbox"
+            name="is_favorite"
+            checked={petData.is_favorite}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <br />
+        <label>
+          Photo:
+          <input
+            type="text"
+            name="photo"
+            value={petData.photo}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Add Pet</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddPet;
